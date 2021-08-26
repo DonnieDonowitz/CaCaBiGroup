f40f7819d96b85e706811d00f059a0ed827aaad2
Matteo Biffoni <s291899@studenti.polito.it>
2021-08-26 16:32:20 +0200

    Small fixes

--- exec.sh
+++ exec.sh
@@ -1,2 +1,2 @@
-g++ -g main.cpp ScreenRecord.cpp $(pkg-config --libs libavformat libavcodec libavdevice libavfilter libavutil libswscale libswresample) -lz -lpthread -o main;
+g++ -g -Wno-all main.cpp ScreenRecord.cpp $(pkg-config --libs libavformat libavcodec libavdevice libavfilter libavutil libswscale libswresample) -lz -lpthread -o main;
 ./main
--- exec_win.sh
+++ exec_win.sh
@@ -1,2 +1,2 @@
-g++ -g main.cpp ScreenRecord.cpp -I /usr/local/include/ $(pkg-config --libs libavformat libavcodec libavdevice libavfilter libavutil libswscale libswresample) -lz -lpthread -o main;
+g++ -g -Wno-all main.cpp ScreenRecord.cpp -I /usr/local/include/ $(pkg-config --libs libavformat libavcodec libavdevice libavfilter libavutil libswscale libswresample) -lz -lpthread -o main;
 ./main.exe
